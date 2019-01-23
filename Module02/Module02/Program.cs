5721cfcf8682b4a64300368004d31a0a552e7042
fcherkez <fatmeibrahimcherkez@gmail.com>
2019-01-23 17:42:28 +0200

    Модул 02, Видео 01 - Добавяне на коментари

---
+++ Module02/Module02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Module02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			// Дефиниране на променливи
+
+
+			//Събиране на числа
+
+
+			//Писане в конзолата
+
+
+
+		}
+	}
+}
