bf491a3452ab96a3cf988b8fa86aaed384e2ee88
fcherkez <fatmeibrahimcherkez@gmail.com>
2019-02-10 20:41:01 +0200

    Модул 03, Видео 02, Работа с double

---
+++ Module03/Module03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Module03
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			double a = 0, b = 0, c = 0;
+
+			Console.Write ("> a = "); a = Convert.ToDouble (Console.ReadLine ());
+			Console.Write ("> b = "); b = Convert.ToDouble (Console.ReadLine ());
+
+
+			Console.WriteLine ("\n\n>a / b = " + (a / b).ToString ());
+
+		}
+	}
+}
