7c005de5444db58fe9b5373af7e871d716776605
fcherkez <fatmeibrahimcherkez@gmail.com>
2019-03-03 19:20:05 +0200

    Модул 05, Видео 01 - Методи

---
+++ Module05/Module05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Module05
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+
+			//Дефиниции
+			string usercommand = "";
+
+
+
+			//Потребителски команди
+			do
+			{
+				Console.Write (" $ ");         usercommand = Console.ReadLine ();
+				if( usercommand.Contains ( " test ")) test ( usercommand );
+
+			}while ( usercommand != "exit") ;
+
+		}
+		public static void test ( string _input)  //test 5 > 5^2
+		{
+			try 
+			{
+				double _temp = 0;
+
+					if ( double.TryParse ( _input.Split (' ')[1], out _temp ) )
+				{
+					Console.WriteLine ( "\nРезултат от " + _temp.ToString () + "^2: " + (_temp * _temp).ToString());
+				}
+			}catch{
+			}
+
+			//Да върнат параметри
+	}
+}
+}
+
