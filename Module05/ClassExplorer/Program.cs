4abf049194b1d187ab24fe0e06e4ea381fd4d62c
fcherkez <fatmeibrahimcherkez@gmail.com>
2019-03-05 12:22:37 +0200

    Модул05, Видео06 - Дефиниране на методи в клас

--- Module05/ClassExplorer/Program.cs
+++ Module05/ClassExplorer/Program.cs
@@ -5,22 +5,40 @@ namespace ClassExplorer
 {
 	public class person 
 	{
-		public string simpleString = "тестова променлива от клас";
-		public string user = "";
+		private string simpleString = "тестова променлива от клас";
+		private string user = "";
 		private string hiddenString = "";
 
 		public person ( string _ini)
 		{
+			getUser (_ini);
+			setWelcomeText ();
+
+		}
+	
+		private void getUser (string _ini)
+		{
 			if (_ini == "password1") hiddenString = "Потребител 1";
 			if (_ini == "password2") hiddenString = "Потребител 2";
 
+			user = hiddenString;
+		}
+
+		private void setWelcomeText ()
+		{
 			if (hiddenString.Length > 0)
 				simpleString = "Добре дошли отново, " + user + "!";
 			else
-			simpleString = "Вие нямате достъп до тази програма";
+				simpleString = "Вие нямате достъп до тази програма";	
+		}
+		public string getWelcomeMessage ()
+		{
+			return simpleString;
 		}
 
 	}
+
+
 
 	class MainClass
 	{
@@ -28,10 +46,14 @@ namespace ClassExplorer
 		public static void Main (string[] args)
 		{
 			Console.Write ("Парола: "); string _user = Console.ReadLine ();
-			//Дефиниране на клас
+
+
+			//Дефиниране на кла
 			person _person = new person(_user);
+
+
 			//Достъпване на клас
-			Console.WriteLine ( _person.simpleString );
+			Console.WriteLine ( _person.getWelcomeMessage () );
 		}
 	}
 }
